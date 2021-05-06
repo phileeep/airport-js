917d4a958c02081280f2efd053d04c0571d59cdf
Phil Smithies <philsmithies@gmail.com>
2021-05-06 17:16:29 +0200

    airport functions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,18 +1,41 @@
 describe("Airport", function(){
 
   let airport;
+  let plane;
   let defaultCapacity = 20
 
   beforeEach(function(){
     airport =  new Airport();
+    plane = new Plane();
   });
 
-  describe('#capacity', function() {
-    it ('automatically sets to default capacity', function() {
+  describe('#capacity', ()=>{
+    it ('automatically sets to default capacity', () => {
       expect(airport.capacity).toEqual(defaultCapacity)
     });
-    it ('Checks that the airport has a hangar to land in', function() {
+    it ('Checks that the airport has a hangar to land in', () => {
       expect(airport.hangar).toEqual([]);
+    });
+    it ('Can land a plane into the hangar', () => {
+      airport.land(plane);
+      expect(airport.hangar).toEqual([plane]);
+    });
+    it ('Can take off a plane from the hangar', () => {
+      airport.land(plane);
+      airport.takeoff();
+      expect(airport.hangar).toEqual([]);
+    });
+    it ('Can have the capacity of the airport changed', () => {
+      CdG = new Airport(1);
+      expect(CdG.capacity).toEqual(1);
+    });
+    it ('Will not land a plane if the airport is full', () => {
+      CdG = new Airport(1);
+      CdG.land(plane);
+      expect(() => { CdG.land(plane); }).toThrowError('Airport is full');
+    }); 
+    it ('Will not take off a plane if the hangar is empty', () => {
+      expect(() => { airport.takeoff(); }).toThrowError('No Planes Available');
     });
   });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -2,7 +2,21 @@ class Airport {
 
     constructor(capacity = 20){
       this.capacity = capacity;
-      this.hangar = []
+      this.hangar = [];
     }
 
+    land(plane) {
+        this.hangar.push(plane)
+        if (this.hangar.length > this.capacity) {
+          throw new Error('Airport is full');
+        } 
+    }
+
+    takeoff() {
+      if (this.hangar.length < 1) {
+        throw new Error('No Planes Available');
+      } else {
+        this.hangar.pop();
+      }
+    }
 }
